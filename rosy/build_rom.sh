611a25c6318a74e1eefb6b005cf172e140ceecd3
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-05-10 08:41:07 +0700

    Rosy: initial build VoltageOS

--- rosy/build_rom.sh
+++ rosy/build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 12.1 .repo/local_manifests
+git clone https://github.com/NFS-projects/local_manifest.git --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
