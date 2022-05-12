20bdf3de428bdfefca6ccb9b845d0792e5afc2d1
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-05-12 16:30:28 +0700

    Test againt Awaken

--- rosy/build_rom.sh
+++ rosy/build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest.git --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -13,9 +13,9 @@ export NFS_BUILD_VARIANT=normal # this is for NFSDev™ only normal or overclock
 # export TARGET_BUILD_GRAPHENEOS_CAMERA=true
 export NFS_ADD_GCAMGO=true
 # export WITH_GMS=true
-lunch voltage_rosy-user
-lunch voltage_rosy-userdebug
+lunch awaken_rosy-user
+lunch awaken_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>Redmi 5 ($device)</b> Building Rom Started%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
-brunch rosy
+make bacon
 # end
 
